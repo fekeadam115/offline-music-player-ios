dede3ffb43230b30070e9aa79f73c9eaaaf67c56
Adam Feke <adam.feke2022@gmail.com>
2023-01-08 19:30:37 -0800

    Created default album art
    Added picture file for default album art and created progress bar for testing seek bar

--- offlineMusicPlayer/offlineMusicPlayer/ContentView.swift
+++ offlineMusicPlayer/offlineMusicPlayer/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        VStack() {
+            
+            Image("default-album")
+                .resizable()
+                .frame(width: 250.0, height: 250.0)
+                .shadow(radius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+                .scaledToFit()
             Text("Hello, world!")
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+            ProgressView(value: 0.02)
+                
+                
+                
         }
         .padding()
     }
