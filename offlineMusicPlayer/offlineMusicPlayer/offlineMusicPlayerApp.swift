59c7705af9d007f48a2d3290faaa18e72f77d683
Adam Feke <adam.feke2022@gmail.com>
2023-01-08 18:54:03 -0800

    Created XCode Project

---
+++ offlineMusicPlayer/offlineMusicPlayer/offlineMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  offlineMusicPlayerApp.swift
+//  offlineMusicPlayer
+//
+//  Created by Adam Feke on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct offlineMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
